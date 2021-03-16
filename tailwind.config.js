7446752b7e0650585f384e0b1096ec2656a96cf7
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-03-16 01:01:49 -0500

    fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./**/*.{jsx,tsx}', './styles/**.css'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
