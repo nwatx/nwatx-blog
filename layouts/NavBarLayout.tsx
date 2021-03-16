67d8882b08e634b669c9a1812a7e3b5f27a8b9ab
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-03-16 17:45:47 -0500

    Update NavBarLayout.tsx

--- layouts/NavBarLayout.tsx
+++ layouts/NavBarLayout.tsx
@@ -5,7 +5,7 @@ export default function NavBarLayout({ children }) {
   return (
     <Navbar>
       <div className="flex flex-grow flex-shrink pb-10 overflow-auto min-h-0 w-full justify-center">
-        <div className="flex flex-grow flex-row w-auto pt-5 pb-10 px-3 md:px-4 justify-center">
+        <div className="flex flex-grow flex-row w-auto mt-5 pb-10 px-3 md:px-4 justify-center">
           {children}
         </div>
       </div>
