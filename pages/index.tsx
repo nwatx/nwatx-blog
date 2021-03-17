2ff39b428bd300efed86d2d7cfd02e615ed827b9
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-03-17 18:50:53 -0500

    seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import NavBarLayout from "../layouts/NavBarLayout";
 import fs from "fs";
 import matter from "gray-matter";
 import BlogPost from "../components/BlogPost";
+import Head from "next/head";
 
 type PostData = {
   author: string;
@@ -11,7 +12,7 @@ type PostData = {
 
 type Post = {
   slug;
-  frontmatter: PostData
+  frontmatter: PostData;
 };
 
 type HomeProps = {
@@ -22,9 +23,25 @@ export default function Home({ posts }: HomeProps) {
   console.log(posts); // show graymatter data
   return (
     <NavBarLayout>
-      <div className='flex flex-col w-full 2xl:w-1/2 space-y-5'>
-        <div className='flex w-full'><p className='text-4xl underline'>Blog</p></div>
-        {posts && posts.map((post) => <BlogPost key={post.slug} slug={post.slug} {...post.frontmatter} />)}
+      <Head>
+        <title>Neo Wang - Blog</title>
+        <meta
+          title="description"
+          content="Neo Wang's blog - code, ping pong, and more!"
+        />
+        <meta
+          name="keywords"
+          content="nwatx, Neo Wang, Next.js, Next.js blog"
+        />
+      </Head>
+      <div className="flex flex-col w-full 2xl:w-1/2 space-y-5">
+        <div className="flex w-full">
+          <p className="text-4xl underline">Blog</p>
+        </div>
+        {posts &&
+          posts.map((post) => (
+            <BlogPost key={post.slug} slug={post.slug} {...post.frontmatter} />
+          ))}
       </div>
     </NavBarLayout>
   );
