525cd511609fd0cb9f0b28674c24637f89ef5151
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-03-20 14:04:09 -0500

    aboutme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,25 @@
 import "tailwindcss/tailwind.css";
-import '../styles/styles.css';
-import '../styles/prism-one-dark.css';
-import Darkmode from 'darkmode-js'
+import "../styles/styles.css";
+import "../styles/prism-one-dark.css";
+import Darkmode from "darkmode-js";
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-
   useEffect(() => {
-    new Darkmode().showWidget();
-  },[])
+    new Darkmode({
+      bottom: "32px", // default: '32px'
+      right: "32px", // default: '32px'
+      left: "unset", // default: 'unset'
+      time: "0.35s", // default: '0.3s'
+      mixColor: "#fff", // default: '#fff'
+      backgroundColor: "#fff", // default: '#fff'
+      buttonColorDark: "#100f2c", // default: '#100f2c'
+      buttonColorLight: "#fff", // default: '#fff'
+      saveInCookies: true, // default: true,
+      label: "🌓", // default: ''
+      autoMatchOsTheme: true, // default: true,
+    }).showWidget();
+  }, []);
 
   return <Component {...pageProps} />;
 }
--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -4,8 +4,8 @@ import NavBarLayout from "../layouts/NavBarLayout";
 export default function aboutme() {
   return (
     <NavBarLayout>
-      <div className="flex w-full justify-center">
-        <div className="flex w-1/2">
+      <div className="flex w-full max-w-7xl px-3 justify-center">
+        <div className="flex w-full">
           <p>
             Hello. I'm Neo. My routine goes something like wake up, sleep,
             repeat. But intermittently, I'll make things like robots, websites,
