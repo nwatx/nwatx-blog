a4c652e095703a16e63c9d0b342d7dc3c4bafc96
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-03-21 15:03:20 -0500

    Update [slug].tsx

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -80,12 +80,12 @@ const BlogPost = ({ source, data }) => {
         <div className="flex w-full max-w-7xl flex-col items-center">
           {/* <div className="flex w-full max-w-7xl p-1 border-b flex-col"> */}
           <div className="flex flex-col w-full justify-center max-w-7xl mt-4 pb-10 md:pb-4 border-b">
-            <div className="text-4xl max-w-7xl flex flex-row w-ful justify-between">
+            <div className="text-4xl max-w-7xl flex flex-row w-ful justify-between items-end">
               <p className="dark:text-gray-100">
                 {data.title}{" "}
               </p>
               <p className="text-base font-light">
-                by <b>{data.author}</b> on{" "}
+                <b>{data.author}</b> on{" "}
                 <b>{new Date(data.date).toLocaleDateString()}</b>
               </p>
             </div>
