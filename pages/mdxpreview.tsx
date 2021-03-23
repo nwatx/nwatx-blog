6d871c3de130090a9f6a213d480a5292621d6790
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-03-23 17:21:25 -0500

    add rich editor

--- pages/mdxpreview.tsx
+++ pages/mdxpreview.tsx
@@ -1,28 +1,14 @@
-import React, {
-  createElement,
-  ReactNode,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import React, { useEffect, useState } from "react";
 import NavBarLayout from "../layouts/NavBarLayout";
-import Prism from "prismjs";
-import { mdx } from "@mdx-js/react";
-import {
-  Editor,
-  LiveEditor,
-  LiveError,
-  LivePreview,
-  LiveProvider,
-} from "react-live";
 import { MdxRemote } from "next-mdx-remote/types";
 import MDXPreview from "../components/MDXPreview";
-import { highlight, languages } from "prismjs/components/prism-core";
+import Editor from "rich-markdown-editor";
 
 export default function mdxpreview({ children }) {
   const [mdxContent, setMdxContent] = useState("");
   const [renderOutput, setRenderOutput] = useState("");
   const [source, setSource] = useState<MdxRemote.Source>();
+  const [richEditor, setRichEditor] = useState(false);
 
   useEffect(() => {
     if (!mdxContent) return;
@@ -47,31 +33,39 @@ export default function mdxpreview({ children }) {
   }, [mdxContent]);
 
   return (
-    <LiveProvider code={renderOutput} scope={{ mdx: mdx }}>
-      <NavBarLayout>
-        <div className="flex flex-row w-full space-x-2 max-w-7xl">
-          {/* <div className="flex w-1/2"> */}
-            <Editor
-              value={mdxContent}
-              onChange={(code) => setMdxContent(code)}
-              cellPadding={10}
-              className='flex w-1/2 bg-gray-800 rounded-md'
-              style={{
-                fontFamily: '"Fira code", "Fira Mono", monospace',
-                fontSize: 14,
-                caretColor: 'white'
-              }}
-            />
+    <NavBarLayout>
+      <div className="flex flex-col w-full space-x-2 max-w-7xl">
+        <div
+          className="flex w-full cursor-pointer"
+          onClick={() => setRichEditor(!richEditor)}
+        >
+          Rich editor: {richEditor ? "on" : "off"}
+        </div>
+        <div className="flex flex-row space-x-3 h-auto">
+          <div className="w-1/2">
+            {richEditor ? (
+              <Editor
+                defaultValue={mdxContent}
+                className="bg-gray-100"
+                onChange={(value) => setMdxContent(value())}
+              />
+            ) : (
+              <textarea
+                defaultValue={mdxContent}
+                className="w-full h-full resize-none bg-gray-800 rounded-l-md outline-none text-white p-5 whitespace-pre-wrap"
+                onChange={(e) => setMdxContent(e.target.value)}
+              />
+            )}
             {/* <textarea
               className="flex p-5 w-full rounded-md resize-y"
               onChange={(e) => setMdxContent(e.target.value)}
             /> */}
-          {/* </div> */}
+          </div>
           <div className="flex flex-wrap overflow-y-auto w-1/2">
             {source && <MDXPreview source={source} />}
           </div>
         </div>
-      </NavBarLayout>
-    </LiveProvider>
+      </div>
+    </NavBarLayout>
   );
 }
