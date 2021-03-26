7563ca463e7b139c2d0515c386ae9ce44c3fef4b
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-03-25 19:28:03 -0500

    semantic naming

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,13 +1,13 @@
 export const h1 = ({...rest}) => {
-    return <p className='text-3xl mt-7 mb-3 font-semibold dark:text-gray-100' {...rest}/>
+    return <h1 className='text-3xl mt-7 mb-3 font-semibold dark:text-gray-100' {...rest}/>
 }
 
 export const h2 = ({...rest}) => {
-    return <p className='text-2xl mt-7 mb-3 font-semibold dark:text-gray-100' {...rest}/>
+    return <h2 className='text-2xl mt-7 mb-3 font-semibold dark:text-gray-100' {...rest}/>
 }
 
 export const h3 = ({...rest}) => {
-    return <p className='text-xl mt-7 mb-3 font-semibold dark:text-gray-100' {...rest} />
+    return <h3 className='text-xl mt-7 mb-3 font-semibold dark:text-gray-100' {...rest} />
 }
 
 export const p = ({...rest}) => {
