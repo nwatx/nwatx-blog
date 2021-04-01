e43921da59e053326ed8900cc7349dc072684a9b
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-04-01 13:51:36 -0500

    Update ProblemCard.tsx

--- components/ProblemCard.tsx
+++ components/ProblemCard.tsx
@@ -17,12 +17,12 @@ export default function ProblemCard({
   const [userAnswer, setUserAnswer] = useState("");
 
   return (
-    <div className="border border-purple-200 dark:border-purple-900 bg-purple-50 dark:bg-purple-900 rounded p-6 my-4 w-full">
+    <div className="border border-purple-200 dark:border-gray-700 bg-purple-50 dark:bg-gray-700 rounded p-6 my-4 w-full">
       <h3 className="dark:text-white font-semibold">{children}</h3>
       <ol className="mt-4 list-decimal" type="A">
         {choices.map((choice) => (
           <li key={choice} className="flex font-medium items-center mb-2">
-            <div onClick={() => {choice === userAnswer ? setUserAnswer("") : setUserAnswer(choice)}} className={`h-4 w-4 rounded-md border-2 cursor-pointer border-gray-400 ${choice === userAnswer && 'bg-gray-400'} mr-2`} />
+            <div onClick={() => {choice === userAnswer ? setUserAnswer("") : setUserAnswer(choice)}} className={`h-4 w-4 rounded-md border-2 cursor-pointer border-gray-600 dark:border-white ${choice === userAnswer && 'bg-gray-600 dark:bg-white'} mr-2`} />
             <p>{choice}</p>
           </li>
         ))}
