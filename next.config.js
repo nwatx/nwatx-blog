6d8c138fcebf9142e259c2125f2d41499c2bafbc
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-06-15 21:28:19 -0500

    next image optimizations further

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	images: {
-		// deviceSizes: [320, 420, 768, 1024, 1200],
+		deviceSizes: [320, 420, 768, 1024, 1200, 1920, 2560, 3840],
 		domains: ["res.cloudinary.com"],
 		loader: 'cloudinary',
 		path: 'https://res.cloudinary.com/dcg5b3jpt/image/upload/'
