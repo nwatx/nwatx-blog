3f197d4f067ae6db8d175388ebcd6c3308ee5fd8
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-07-30 19:09:20 -0500

    upd typo color

--- tailwind.config.js
+++ tailwind.config.js
@@ -77,7 +77,7 @@ module.exports = {
 							color: theme("colors.gray.900"),
 						},
 						code: {
-							color: theme("colors.pink.500"),
+							color: theme("colors.blue.500"),
 							backgroundColor: theme("colors.gray.100"),
 							paddingLeft: "4px",
 							paddingRight: "4px",
