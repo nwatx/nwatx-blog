6169a176b40190a4dd5d970c4c9f725d1d0af868
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-08-22 16:33:44 -0500

    some light formatting

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -61,7 +61,6 @@ export const MDXComponents = {
 };
 
 const BlogPost = ({ source, data }) => {
-	// const content = hydrate(source, { components: MDXComponents });
 	const router = useRouter();
 
 	useEffect(() => {
@@ -80,20 +79,19 @@ const BlogPost = ({ source, data }) => {
 	// }, [ref.current]);
 
 	// split a string by whitespace
-
 	const split = (str) => {
 		return str.split(/\s+/);
 	};
 
+
+	// takes the last two elements if the array >= 4 elements, making them blue
 	let titleFirstPart;
 	if (data.title) titleFirstPart = split(data.title);
 	let titleSecondPart;
 	if (titleFirstPart && titleFirstPart.length >= 4) titleSecondPart = titleFirstPart?.splice(-2)?.join(" ");
 	titleFirstPart = titleFirstPart.join(" ");
-	console.log(titleFirstPart, titleSecondPart);
 
 	return (
-		// <MDXProvider>
 		<>
 			<Head>
 				<title>
@@ -106,7 +104,6 @@ const BlogPost = ({ source, data }) => {
 			</Head>
 			<NavBarLayout>
 				<div className="flex xl:ml-2 w-full flex-col items-center">
-					{/* <div className="flex w-full max-w-7xl p-1 border-b flex-col"> */}
 					<div className="flex flex-col w-full justify-center max-w-3xl mt-4 pb-10 md:pb-4 border-b my-4">
 						<div className="text-4xl max-w-3xl flex flex-row w-full flex-wrap items-end">
 							<p className="dark:text-gray-100 font-extrabold text-5xl">
@@ -114,11 +111,6 @@ const BlogPost = ({ source, data }) => {
 							</p>
 						</div>
 						<div className="flex items-end flex-row flex-wrap w-full justify-between">
-							{/* {data.description && (
-                <h2 className="text-xl mt-1 text-gray-500 dark:text-gray-300">
-                  {data.description}
-                </h2>
-              )} */}
 							{data.author && data.date && (
 								<div className="flex mt-2 flex-row w-full flex-wrap justify-between align-text-bottom">
 									<p className="text-sm text-gray-600 dark:text-gray-400">
@@ -135,18 +127,13 @@ const BlogPost = ({ source, data }) => {
 							)}
 						</div>
 					</div>
-					{/* <div className="flex justify-center mt-5 pb-10 md:pb-4 w-full"> */}
-					{/* <div className="flex w-full flex-col relative"> */}
 					<article className="prose overflow-x-auto dark:prose-dark w-full my-7 max-w-3xl mx-0">
 						{/* {content} */}
 						<MDXRemote {...source} components={MDXComponents} />
 					</article>
-					{/* </div> */}
-					{/* </div> */}
 				</div>
 			</NavBarLayout>
 		</>
-		// </MDXProvider>
 	);
 };
 
@@ -190,9 +177,6 @@ export async function getStaticProps({ params: { slug } }) {
 				require("rehype-katex"),
 				require("rehype-slug"),
 				require("@mapbox/rehype-prism"),
-				// require("@jsdevtools/rehype-toc"),
-				// require('rehype-autolink-headings'),
-				// require('rehype-sanitize')
 			],
 		},
 		scope: {
@@ -204,28 +188,12 @@ export async function getStaticProps({ params: { slug } }) {
 		props: {},
 	};
 
-	// const convertedSource = {
-	//   ...source,
-	//   renderedOutput: source.renderedOutput.split("[slug]").join(slug as string),
-	//   compiledSource: source.compiledSource.split("[slug]").join(slug as string),
-	// };
-
-	// console.log(source);
-
 	const frontmatter = {
 		...data,
 		date: data.date.toISOString(),
 	};
 
 	frontmatter["author"] = frontmatter["author"] || "Neo Wang";
-
-	// source.renderedOutput = source.renderedOutput.replaceAll('<!-- -->', '');
-
-	// source.renderedOutput = source.renderedOutput.split("<!-- -->").join("");
-
-	// console.log(source.renderedOutput);
-
-	// console.log(markdownWithMetadata);
 
 	return {
 		props: {
