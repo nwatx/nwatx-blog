c96e19f9e81b9a9494dce207bc034d416a4ee761
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-09-20 20:14:08 -0500

    fix nav

--- layouts/NavBarLayout.tsx
+++ layouts/NavBarLayout.tsx
@@ -6,7 +6,7 @@ import Navbar from "../components/Navbar";
 export default function NavBarLayout({ children }) {
 	return (
 		<Navbar>
-			<div className="flex flex-col h-full flex-shrink flex-grow min-h-0 w-full justify-center">
+			<div className="flex h-full flex-shrink flex-grow min-h-0 w-full justify-center">
 				<div className="flex w-full pb-5 md:pb-2 flex-row mt-5 px-2 md:px-4 justify-center">
 					{children}
 				</div>
