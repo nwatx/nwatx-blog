a4d7c1a829f6cf18f83181e0aaa7b467296bb9e5
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-11-05 14:10:25 -0500

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,14 +1,16 @@
-import admin from 'firebase-admin';
+import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL
-    }),
-    databaseURL: 'https://nwatx-me-default-rtdb.firebaseio.com'
-  });
+	admin.initializeApp({
+		credential: admin.credential.cert({
+			projectId: process.env.FIREBASE_PROJECT_ID,
+			privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+		}),
+		databaseURL: "https://nwatx-me-default-rtdb.firebaseio.com",
+	});
+} else {
+	admin.app();
 }
 
 const db = admin.database();
