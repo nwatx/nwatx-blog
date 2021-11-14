a2d5cf8424417204f10552ed1e0012286a6f2c89
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-11-14 15:51:57 -0600

    add nifty images

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -1,75 +1,104 @@
+import buildUrl from "cloudinary-build-url";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
+import NextImage from "./NextImage";
 import { TagColor } from "./TagColors";
 
 export type BlogPostProps = {
-  title: string;
-  author: string;
-  description: string;
-  slug: string;
-  date: Date;
-  tags?: string;
-  image?: string;
+	title: string;
+	author: string;
+	description: string;
+	slug: string;
+	date: Date;
+	tags?: string;
+	image?: string;
 };
 
 export default function BlogPost({
-  title,
-  slug,
-  author,
-  description,
-  date,
-  image,
-  tags,
+	title,
+	slug,
+	author,
+	description,
+	date,
+	image,
+	tags,
 }: BlogPostProps) {
-  return (
-    <div className="flex w-full justify-between pt-3 pb-5 pr-10 rounded-lg antialiased">
-      <div className="flex flex-col space-y-2 w-full">
-        {tags && (
-          <div className="flex flex-wrap items-baseline w-full mt-1 mb-2 space-y-1">
-            {tags
-              .split(",")
-              .sort((a, b) => a.trim().localeCompare(b.trim()))
-              .map((tag) => (
-                <div
-                  key={tag}
-                  className={`rounded-md dark:bg-gray-800 dark:text-white mr-2 ${
-                    TagColor[tag] && TagColor[tag][0]
-                  }`}
-                >
-                  <p
-                    className={`uppercase m-0 text-xs dark:text-white py-1 px-2 ${
-                      TagColor[tag] && TagColor[tag][1]
-                    }`}
-                  >
-                    {tag}
-                  </p>
-                </div>
-              ))}
-          </div>
-        )}
-        <Link href={`/post/${slug}`}>
-          <div className="flex flex-col sm:flex-row cursor-pointer">
-            <p className="text-3xl font-semibold text-gray-800 dark:text-gray-100 hover:underline">{title}</p>
-            {author && (
-              <div className="mx-1 flex h-full w-full sm:w-auto">
-                <p className="text-md text-gray-600 dark:text-gray-400 self-end font-light">
-                  by <b className="font-semibold">{author}</b> on{" "}
-                  <b className='font-semibold'>{date && new Date(date).toLocaleDateString()}</b>
-                </p>
-              </div>
-            )}
-          </div>
-        </Link>
-        <div>
-          <p className="text-md text-gray-800 dark:text-gray-200">
-            {description}
-          </p>
-        </div>
-      </div>
-      <div className="flex flex-col justify-center min-h-full">
-        <p className="text-3xl font-light animate-pulse">{">"}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-row w-full justify-between pt-3 pb-5 pr-10 rounded-lg antialiased">
+			{image && (
+				<div className="w-36 mr-10 h-auto">
+					<NextImage
+            className='rounded-xl'
+						src={buildUrl(image, {
+							cloud: {
+								cloudName: "dcg5b3jpt",
+							},
+							transformations: {
+								resize: {
+									type: "scale",
+									width: 500,
+									height: 500,
+								},
+							},
+						})}
+						noLoader
+						width={500}
+						height={500}
+						alt={title}
+					/>
+				</div>
+			)}
+			<div className="flex flex-col space-y-2 w-full">
+				{tags && (
+					<div className="flex flex-wrap items-baseline w-full mt-1 mb-2 space-y-1">
+						{tags
+							.split(",")
+							.sort((a, b) => a.trim().localeCompare(b.trim()))
+							.map((tag) => (
+								<div
+									key={tag}
+									className={`rounded-md dark:bg-gray-800 dark:text-white mr-2 ${
+										TagColor[tag] && TagColor[tag][0]
+									}`}
+								>
+									<p
+										className={`uppercase m-0 text-xs dark:text-white py-1 px-2 ${
+											TagColor[tag] && TagColor[tag][1]
+										}`}
+									>
+										{tag}
+									</p>
+								</div>
+							))}
+					</div>
+				)}
+				<Link href={`/post/${slug}`}>
+					<div className="flex flex-col sm:flex-row cursor-pointer">
+						<p className="text-3xl font-semibold text-gray-800 dark:text-gray-100 hover:underline">
+							{title}
+						</p>
+						{author && (
+							<div className="mx-1 flex h-full w-full sm:w-auto">
+								<p className="text-md text-gray-600 dark:text-gray-400 self-end font-light">
+									by <b className="font-semibold">{author}</b> on{" "}
+									<b className="font-semibold">
+										{date && new Date(date).toLocaleDateString()}
+									</b>
+								</p>
+							</div>
+						)}
+					</div>
+				</Link>
+				<div>
+					<p className="text-md text-gray-800 dark:text-gray-200">
+						{description}
+					</p>
+				</div>
+			</div>
+			<div className="flex flex-col justify-center min-h-full">
+				<p className="text-3xl font-light animate-pulse">{">"}</p>
+			</div>
+		</div>
+	);
 }
--- components/NextImage.tsx
+++ components/NextImage.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
 import { buildUrl } from "cloudinary-build-url";
 
+type NextImageProps = {
+	src?: string;
+	blurred?: boolean;
+	noLoader?: boolean;
+	layout?: "responsive" | "fixed" | "fill" | "intrinsic";
+	[x: string]: any;
+};
+
 export default function NextImage({
 	blurred = true,
 	noLoader = false,
+	layout = "responsive",
 	...props
-}) {
+}: NextImageProps) {
 	// if the blurred prop is on, apply cloudinary image transformation to blur the image
 	const blurredUrl =
 		blurred &&
@@ -29,7 +38,7 @@ export default function NextImage({
 			<Image
 				placeholder={blurred ? "blur" : "empty"}
 				blurDataURL={blurredUrl}
-				layout="responsive"
+				layout={layout}
 				loader={normalLoader}
 				{...props}
 			/>
@@ -41,7 +50,7 @@ export default function NextImage({
 		<Image
 			placeholder={blurred ? "blur" : "empty"}
 			blurDataURL={blurredUrl}
-			layout="responsive"
+			layout={layout}
 			{...props}
 		/>
 	);
